bf982f3241f5229a6b845fabee8fe3ee99cb3ea6
Leo Techs <FRC4459@gmail.com>
2023-03-01 16:23:42 -0500

    Fix (?) Mechanum quadrant II & III

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,22 +8,23 @@ import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-public class Drivetrain extends SubsystemBase 
-{
-    private final Talon rightMotor = new Talon(Constants.rightMotorPort);
-    private final Talon rightMotorFollower = new Talon(Constants.rightMotorFollowerPort);
-    private final Talon leftMotor = new Talon(Constants.leftMotorPort);
-    private final Talon leftMotorFollower = new Talon(Constants.leftMotorFollowerPort);
+public class Drivetrain extends SubsystemBase {
+  private final Talon rightMotor = new Talon(Constants.rightMotorPort);
+  private final Talon rightMotorFollower = new Talon(Constants.rightMotorFollowerPort);
+  private final Talon leftMotor = new Talon(Constants.leftMotorPort);
+  private final Talon leftMotorFollower = new Talon(Constants.leftMotorFollowerPort);
 
-    private final MotorControllerGroup motorGroupLeft = new MotorControllerGroup(leftMotor, leftMotorFollower);
-    private final MotorControllerGroup motorGroupRight = new MotorControllerGroup(rightMotor, rightMotorFollower);
+  private final MotorControllerGroup motorGroupLeft =
+      new MotorControllerGroup(leftMotor, leftMotorFollower);
+  private final MotorControllerGroup motorGroupRight =
+      new MotorControllerGroup(rightMotor, rightMotorFollower);
 
-    private final DifferentialDrive diffDrive = new DifferentialDrive(motorGroupLeft, motorGroupRight);
+  private final DifferentialDrive diffDrive =
+      new DifferentialDrive(motorGroupLeft, motorGroupRight);
 
-    private final double speedMult = 0.8;
+  private final double speedMult = 0.8;
 
-  public Drivetrain() 
-  {
+  public Drivetrain() {
     rightMotor.setSafetyEnabled(true);
     rightMotorFollower.setSafetyEnabled(true);
     leftMotor.setSafetyEnabled(true);
@@ -34,30 +35,30 @@ public class Drivetrain extends SubsystemBase
   }
 
 
-  public boolean stopped() 
+  public boolean stopped()
   {
-    if (Utility.allEqual(getAllMotors(), 0)) 
+    if (Utility.allEqual(getAllMotors(), 0))
     {
       return true;
     }
     return false;
   }
 
-  public void setLeft(double i) 
+  public void setLeft(double i)
   {
     i = i * speedMult;
     leftMotor.set(i);
     leftMotorFollower.set(i / 2);
   }
-  
-  public void setRight(double i) 
+
+  public void setRight(double i)
   {
     i = i * speedMult;
     rightMotor.set(i);
     rightMotorFollower.set(i);
   }
 
-  public void set(double i) 
+  public void set(double i)
   {
     i = i * speedMult;
     rightMotor.set(i);
@@ -74,7 +75,8 @@ public class Drivetrain extends SubsystemBase
     leftMotorFollower.stopMotor();
   }
 
-  public void Drive(String mode, Double axis1, Double axis2) {
+  public void Drive(String mode, Double axis1, Double axis2)
+  {
     // Flip the values going to the testbed, because otherwise it would drive backwards.
     axis1 = -axis1;
     axis2 = -axis2;
@@ -82,7 +84,7 @@ public class Drivetrain extends SubsystemBase
     axis1 *= speedMult;
     axis2 *= speedMult;
 
-    if (mode == "Arcade") 
+    if (mode == "Arcade")
     {
       diffDrive.arcadeDrive(axis1, axis2);
     }
@@ -94,7 +96,8 @@ public class Drivetrain extends SubsystemBase
   }
 
 
-  public void mechanumDrive(CommandXboxController controller) {
+  public void mechanumDrive(CommandXboxController controller)
+  {
     double forwardBack = controller.getLeftY();
     double left = controller.getLeftTriggerAxis();
     double right = controller.getRightTriggerAxis();
@@ -109,63 +112,79 @@ public class Drivetrain extends SubsystemBase
 
     forwardBack = -forwardBack;
 
-      if (forwardBack > 0.05 || forwardBack < -0.05)
-      {
-        System.out.print(forwardBack);
-        System.out.print("\n");
-        setLeft(forwardBack);
-        setRight(forwardBack);
-      }
+    if (forwardBack > 0.05 || forwardBack < -0.05)
+    {
+      System.out.print(forwardBack);
+      System.out.print("\n");
+      setLeft(forwardBack);
+      setRight(forwardBack);
+    }
 
-      if (left > 0.05)
-      {
-        setLeft(-left);
-        setRight(left);
-      }
+    if (left > 0.05)
+    {
+      setLeft(-left);
+      setRight(left);
+    }
 
-      if (right > 0.05) 
-      {
-        setLeft(right);
-        setRight(-right);
-      }
+    if (right > 0.05)
+    {
+      setLeft(right);
+      setRight(-right);
+    }
 
-      if ((shiftX > 0.12 || shiftX < -0.12) || (shiftY > 0.12 || shiftY < -0.12)) 
+    if ((shiftX > 0.12 || shiftX < -0.12) || (shiftY > 0.12 || shiftY < -0.12))
+    {
+      double desiredAngle = Math.atan(shiftY / shiftX) - 45;
+      double mg1, mg2;
+      if (shiftX > 0)
       {
-        double desiredAngle = Math.atan(shiftY / shiftX) - 45;
-        double mg1, mg2;
-        if (shiftX > 0) {
+
+        if (shiftY < 0)
+        {
+          mg1 = Math.cos(desiredAngle);
+          mg2 = Math.sin(-desiredAngle);
+        }
+        else
+        {
           mg1 = Math.cos(desiredAngle);
           mg2 = Math.sin(desiredAngle);
-        } else {
-          if (shiftY < 0) 
-          {
-            mg1 = Math.cos(desiredAngle);
-            mg2 = Math.sin(-desiredAngle); 
-          } else {
-            mg1 = Math.cos(-desiredAngle);
-            mg2 = Math.sin(-desiredAngle);
-          }
         }
-        rightMotor.set(mg2);
-        rightMotorFollower.set(mg1);
-        leftMotor.set(mg1);
-        leftMotorFollower.set(mg2);
       }
+      
+      else
+      {
+        if (shiftY < 0)
+        {
+          mg1 = Math.cos(-desiredAngle);
+          mg2 = Math.sin(-desiredAngle);
+        }
+        else
+        {
+          mg1 = Math.cos(-desiredAngle);
+          mg2 = Math.sin(desiredAngle);
+        }
+      }
+      
+      rightMotor.set(mg2);
+      rightMotorFollower.set(mg1);
+      leftMotor.set(mg1);
+      leftMotorFollower.set(mg2);
+    }
   }
 
-  private double[] getAllMotors() 
+  private double[] getAllMotors()
   {
     double[] arr = new double[4];
-    
-    arr[0] = rightMotor.get() ;
+
+    arr[0] = rightMotor.get();
     arr[1] = rightMotorFollower.get();
     arr[2] = leftMotor.get();
     arr[3] = leftMotorFollower.get();
-    
+
     return arr;
   }
 
-  private void feedMotors() 
+  private void feedMotors()
   {
     leftMotor.feed();
     leftMotorFollower.feed();
@@ -175,13 +194,13 @@ public class Drivetrain extends SubsystemBase
 
 
   @Override
-  public void periodic() 
+  public void periodic()
   {
     feedMotors();
-    }
+  }
 
   @Override
-  public void simulationPeriodic() 
+  public void simulationPeriodic()
   {
     // This method will be called once per scheduler run during simulation
   }
